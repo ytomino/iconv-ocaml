61af7f75ffbaffcd096f085cbcc2e744a9eddbdc
ytomino <demoonlit@panathenaia.halfmoon.jp>
2023-02-17 10:53:05 +0900

    Print "ok" to stderr in examples

--- examples/test_conv.ml
+++ examples/test_conv.ml
@@ -1,6 +1,16 @@
-(* #load "iconv.cma";; *)
+let verbose = Array.length Sys.argv > 1 && Sys.argv.(1) = "--verbose";;
+
 open Iconv;;
 
 let c = iconv_open ~tocode:"sjis" ~fromcode:"utf-8";;
 let s = "ソースコード直書きのUTF-8文字列です\n" in
-print_string (iconv c s);;
+let x = iconv c s in
+if verbose then print_endline (String.escaped x);
+assert (
+	x = "\131\\\129[\131X\131R\129[\131h\146\188\143\145\130\171\130\204UTF-8"
+		^ "\149\182\142\154\151\241\130\197\130\183\n"
+);;
+
+(* report *)
+
+Printf.eprintf "ok\n";;
--- examples/test_pp.ml
+++ examples/test_pp.ml
@@ -1,5 +1,12 @@
-(* #load "iconv.cma";; *)
+let verbose = Array.length Sys.argv > 1 && Sys.argv.(1) = "--verbose";;
+
 open Iconv;;
 open Iconv_pp;;
 let c = iconv_open ~tocode:"sjis" ~fromcode:"utf-8";;
-Format.printf "%a@." pp_iconv c;;
+let x = Format.asprintf "%a@." pp_iconv c in
+if verbose then Printf.printf "%s\n" (String.escaped x);
+assert (x = "<to SHIFT_JIS from UTF-8>\n");;
+
+(* report *)
+
+Printf.eprintf "ok\n";;
