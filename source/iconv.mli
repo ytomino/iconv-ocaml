a0676d54337755c17d5d8967efd870406a027ed2
ytomino <demoonlit@panathenaia.halfmoon.jp>
2023-02-17 14:06:41 +0900

    Add the functions to get the GNU libiconv version

--- source/iconv.mli
+++ source/iconv.mli
@@ -1,3 +1,9 @@
+external iconv_get_version_opt: unit -> (int * int) option =
+	"mliconv_get_version_opt"
+(** Return [Some (minor, major)] if GNU libiconv is used, otherswise [None]. *)
+
+val iconv_get_version_string_opt: unit -> string option
+
 type iconv_t
 
 external iconv_open: tocode:string -> fromcode:string -> iconv_t =
