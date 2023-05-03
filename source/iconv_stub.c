7b66296589b7b44bf6a0cb35c4399b228d1cf1a9
ytomino <demoonlit@panathenaia.halfmoon.jp>
2023-05-04 05:47:31 +0900

    Change the comparison support of iconv_t to optional

--- source/iconv_stub.c
+++ source/iconv_stub.c
@@ -50,8 +50,10 @@ static void get_substitute(
 	size_t *substitute_length);
 
 static void mliconv_finalize(value r);
+#if defined(SUPPORT_COMPARISON)
 static int mliconv_compare(value left, value right);
 static long mliconv_hash(value data);
+#endif
 #if defined(SUPPORT_SERIALIZATION)
 static void mliconv_serialize(
 	value v, unsigned long *wsize_32, unsigned long *wsize_64);
@@ -61,8 +63,13 @@ static unsigned long mliconv_deserialize(void *dst);
 static struct custom_operations iconv_ops = {
 	.identifier = "jp.halfmoon.panathenaia.iconv",
 	.finalize = mliconv_finalize,
+#if defined(SUPPORT_COMPARISON)
 	.compare = mliconv_compare,
 	.hash = mliconv_hash,
+#else
+	.compare = custom_compare_default,
+	.hash = custom_hash_default,
+#endif
 #if defined(SUPPORT_SERIALIZATION)
 	.serialize = mliconv_serialize,
 	.deserialize = mliconv_deserialize};
@@ -80,6 +87,8 @@ static void mliconv_finalize(value data)
 	caml_stat_free(internal->fromcode);
 	CAMLreturn0;
 }
+
+#if defined(SUPPORT_COMPARISON)
 
 static int mliconv_compare(value left, value right)
 {
@@ -115,6 +124,8 @@ static long mliconv_hash(value data)
 	long result = (strlen(internal->tocode) << 4) + strlen(internal->fromcode);
 	CAMLreturnT(long, result);
 }
+
+#endif
 
 #if defined(SUPPORT_SERIALIZATION)
 
