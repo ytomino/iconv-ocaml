0b697f8f9352701e883e714b5dcf4119693fd52f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-18 19:32:15 +0900

    Delete the result type of the function

--- source/iconv_pp.ml
+++ source/iconv_pp.ml
@@ -3,6 +3,6 @@ open Iconv;;
 external tocode: iconv_t -> string = "mliconv_tocode";;
 external fromcode: iconv_t -> string = "mliconv_fromcode";;
 
-let pp_iconv (f: Format.formatter) (conv: iconv_t): unit = (
+let pp_iconv (f: Format.formatter) (conv: iconv_t) = (
 	Format.fprintf f "<to %s from %s>" (tocode conv) (fromcode conv)
 );;
