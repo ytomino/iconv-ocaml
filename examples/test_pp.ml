b83e95d10a1c9c8d7e32ce94fee7f2bbf5e96890
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-02-23 16:51:53 +0900

    Change the output of the pretty printer
    Use record-like syntax but "<"...">" for meaning abstract type instead
    of "{"..."}".
    
    Quoted strings is better because tocode or fromcode can be empty.
    Additionally, output substitute and force_substitute.

--- examples/test_pp.ml
+++ examples/test_pp.ml
@@ -3,9 +3,21 @@ let verbose = Array.length Sys.argv > 1 && Sys.argv.(1) = "--verbose";;
 open Iconv;;
 open Iconv_pp;;
 let c = iconv_open ~tocode:"sjis" ~fromcode:"utf-8";;
+set_force_substitute c true;
 let x = Format.asprintf "%a@." pp_iconv c in
 if verbose then Printf.printf "%s\n" (String.escaped x);
-assert (x = "<to SHIFT_JIS from UTF-8>\n" || x = "<to sjis from utf-8>\n");;
+assert (
+	x = "<\
+		tocode = \"SHIFT_JIS\"; \
+		fromcode = \"UTF-8\"; \
+		substitute = \"?\";\n \
+		force_substitute = true>\n"
+	|| x = "<\
+		tocode = \"sjis\"; \
+		fromcode = \"utf-8\"; \
+		substitute = \"?\";\n \
+		force_substitute = true>\n"
+);;
 
 (* report *)
 
--- source/iconv_pp.ml
+++ source/iconv_pp.ml
@@ -4,5 +4,13 @@ external tocode: iconv_t -> string = "mliconv_tocode";;
 external fromcode: iconv_t -> string = "mliconv_fromcode";;
 
 let pp_iconv (f: Format.formatter) (conv: iconv_t) = (
-	Format.fprintf f "<to %s from %s>" (tocode conv) (fromcode conv)
+	Format.fprintf f
+		"<@[\
+			tocode = \"%s\";@ \
+			fromcode = \"%s\";@ \
+			substitute = \"%s\";@ \
+			force_substitute = %b\
+		@]>"
+		(String.escaped (tocode conv)) (String.escaped (fromcode conv))
+		(String.escaped (substitute conv)) (force_substitute conv)
 );;
