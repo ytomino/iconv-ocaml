5ae869906cdb795c57d8bf1313d6ecc567bc523c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-03-14 02:10:59 +0900

    Rename Iconv.Out_iconv.out_iconv to t, following Out_channel.t.
    This is a breaking change.

--- source/iconv__Out_iconv.mli
+++ source/iconv__Out_iconv.mli
@@ -1,12 +1,12 @@
 open Iconv
 
 type out_state
-type out_iconv = iconv_t * out_state
+type t = iconv_t * out_state
 
 val open_out: tocode:string -> fromcode:string ->
-	(string -> int -> int -> unit) -> out_iconv
-val output_substring: out_iconv -> string -> int -> int -> unit
-val output_string: out_iconv -> string -> unit
-val flush: out_iconv -> unit
-val end_out: out_iconv -> unit
-val reset_out: out_iconv -> unit
+	(string -> int -> int -> unit) -> t
+val output_substring: t -> string -> int -> int -> unit
+val output_string: t -> string -> unit
+val flush: t -> unit
+val end_out: t -> unit
+val reset_out: t -> unit
