6f7ec56cfe821a3f63b264366385dfd08481277c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-10-11 03:33:22 +0900

    Use the signature of Longident in `ocamlc -where`/compiler-libs

--- source/iconv_pp_install.ml
+++ source/iconv_pp_install.ml
@@ -1,9 +1,3 @@
-module Longident = struct
-	type t =
-		| Lident of string
-		| Ldot of t * string
-		| Lapply of t * t;;
-end;;
 open Longident;;
 Topdirs.dir_install_printer Format.std_formatter
-	(Obj.magic (Ldot (Lident "Iconv_pp", "pp_iconv")));;
+	(Ldot (Lident "Iconv_pp", "pp_iconv"));;
